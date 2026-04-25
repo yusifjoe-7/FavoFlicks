aba94234be3f456481e7c391a7619ff79c59043a
yusifjoe7 <yusifjoe7@gmail.com>
2026-04-25 09:10:05 +0300

    wep app sec try

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,6 @@ import {HelmetProvider} from 'react-helmet-async'
 
 
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => console.log("SW registered:", reg))
-      .catch((err) => console.log("SW failed:", err));
-  });
-}
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <FavoritesProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({registerType: 'autoUpdate',
+      includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
+      manifest: {
+        name: 'FavoFlicks films',
+        short_name: 'FavoFlicks',
+        theme_color: '#6366F1',
+        background_color: '#0F1115',
+        display: 'standalone',
+        "icons": [
+    {
+      "src": "/icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "/icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+      },
+    })],
 })
