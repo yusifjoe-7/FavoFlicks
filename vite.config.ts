f021966b95a013ab537381fed241497efb52c381
yusifjoe7 <yusifjoe7@gmail.com>
2026-05-06 22:57:57 +0300

    theme color tyni

--- vite.config.ts
+++ vite.config.ts
@@ -36,19 +36,9 @@ export default defineConfig({
         background_color: '#0F1115',
         display: 'standalone',
         "icons": [
-    {
-      "src": "/icons/icon-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "any maskable"
-    },
-    {
-      "src": "/icons/icon-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png",
-      "purpose": "any maskable"
-    }
-  ]
+  { "src": "/icons/icon-192.svg", "sizes": "192x192", "type": "image/svg+xml" },
+  { "src": "/icons/icon-512.svg", "sizes": "512x512", "type": "image/svg+xml" }
+]
       },
     })],
 })
