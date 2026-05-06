187769eb4234eca51f6859499fbe8b080837d1a3
yusifjoe7 <yusifjoe7@gmail.com>
2026-05-06 20:53:48 +0300

    fix 1

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -4,9 +4,10 @@ import SkeletonCard from "../components/skeletonCard";
 import { useState, useEffect } from "react";
 import { getTrendingDay, getTopRated, getDiscover} from "../API/discoverApi";
 import type { movie } from "../types/types";
-
+import { useNavigate } from "react-router-dom";
 export default function Discover() {
  
+   const navigate = useNavigate();
 
       const [Amovies, setAmovies] = useState<movie[]>([]);
       const [Fmovies, setFmovies] = useState<movie[]>([]);
@@ -17,11 +18,8 @@ export default function Discover() {
       // const [Tmovies, setTmovies] = useState<movie[]>([]);
       // const [ADmovies, setADmovies] = useState<movie[]>([]);
 
-      const [loading1, setLoading1] = useState(true);
-       const [loading2, setLoading2] = useState(true);
-       const [loading3, setLoading3] = useState(true);
-       const [loading4, setLoading4] = useState(true);
-       const [loading5, setLoading5] = useState(true);
+      const [loading, setLoading] = useState(true);
+      
        // const [loading6, setLoading6] = useState(true);
 
 
@@ -29,42 +27,38 @@ export default function Discover() {
 
    useEffect(() => {
       const fetchMovies = async () => {
-        setLoading1(true);
-       setLoading2(true)
-       setLoading3(true)
-       setLoading4(true)
-       setLoading5(true)
-       // setLoading6(true)
+        setLoading(true);
+      
+        try{
+          const [Adata, Fdata, Ddata, Hdata, Sdata] = await Promise.all([
+            getTrendingDay(),
+            getDiscover("movie"),
+            getDiscover("tv"),
+            getTopRated("movie"),
+            getTopRated("tv")
+          ]);
 
+          if(!Adata || !Fdata || !Ddata || !Hdata || !Sdata) throw new Error("Failed to fetch movies");
 
-         
-         const Adata= await getTrendingDay();
+          
         setAmovies(Adata);
-        setLoading1(false);
-
-
-          const Fdata= await getDiscover("movie");
+      
+          
           setFmovies(Fdata);
-          setLoading2(false);
-
-
-          const Ddata= await getDiscover("tv");
+         
           setDmovies(Ddata);
-          setLoading3(false);
-
-
-          const Hdata= await getTopRated("movie");
-          setHmovies(Hdata);
-          setLoading4(false);
-
-
-
-          const Sdata= await getTopRated("tv");
+                  
+          setHmovies(Hdata);      
+         
           setSmovies(Sdata);
-          setLoading5(false);
+          
+          setLoading(false)
+        }catch(err){
+          console.log(err)
+          navigate("/not-found");
+          setLoading(false);
+        }
 
-
-        
 
         
         
@@ -84,21 +78,21 @@ export default function Discover() {
   <div className="min-h-screen bg-bg py-5">
     <span className="block text-2xl sm:text-3xl font-bold ml-10 sm:ml-20 sm:mt-3 mt-5 sm:mb-10 mb-5">- trending <span className="curseve text-accent">today</span></span>
     <SideScroll>
-     {loading1
+     {loading
                  ? Array.from({ length: skeletonLenth }).map((_, i) => <SkeletonCard key={i} />)
                  : Amovies.map((m: movie) => <Card key={m.id} data={m} />)
                }
     </SideScroll>
     <span className="block text-2xl sm:text-3xl font-bold ml-10 sm:ml-20 sm:mb-10 my-5">- top rated <span className="curseve text-accent">movies</span></span>
     <SideScroll>
-     {loading2
+     {loading
                  ? Array.from({ length: skeletonLenth }).map((_, i) => <SkeletonCard key={i} />)
                  : Hmovies.map((m: movie) => <Card key={m.id} data={m} index="movie" />)
                }
     </SideScroll>
     <span className="block text-2xl sm:text-3xl font-bold ml-10 sm:ml-20 sm:mb-10 my-5">- top rated <span className="curseve text-accent">tv shows</span></span>
     <SideScroll>
-     {loading3
+     {loading
                  ? Array.from({ length: skeletonLenth }).map((_, i) => <SkeletonCard key={i} />)
                  : Smovies.map((m: movie) => <Card key={m.id} data={m} index="tv" />)
                }
@@ -106,14 +100,14 @@ export default function Discover() {
 
     <span className="block text-2xl sm:text-3xl font-bold ml-10 sm:ml-20 sm:mb-10 my-5">- discover <span className="curseve text-accent">movies</span></span>
 <SideScroll>
-     {loading4
+     {loading
                  ? Array.from({ length: skeletonLenth }).map((_, i) => <SkeletonCard key={i} />)
                  : Fmovies.map((m: movie) => <Card key={m.id} data={m} index="movie" />)
                }
     </SideScroll>
     <span className="block text-2xl sm:text-3xl font-bold ml-10 sm:ml-20 sm:mb-10 my-5">- discover <span className="curseve text-accent">tv shows</span></span>
 <SideScroll>
-     {loading5
+     {loading
                  ? Array.from({ length: skeletonLenth }).map((_, i) => <SkeletonCard key={i} />)
                  : Dmovies.map((m: movie) => <Card key={m.id} data={m} index="tv" />)
                }
